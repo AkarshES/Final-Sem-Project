d59f3f73154d570e1c9f63d9c1b4ecd4a1f50274
Akarsh E S <akarsh.es@gmail.com>
2013-03-29 18:21:09 +0530

    Code to load log data into MongoDB. Started with class implementation for project. DB collection names are currently hard coded.

--- parse_apache.py
+++ parse_apache.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
 import re
 import os
-import dateutil.parser 
+import dateutil.parser
+from datetime import datetime
+from pymongo import MongoClient
 
-for i in range(1,5):
-	log_file = open("access_log_"+str(i),"r")
-	regex = '([(\da-zA-Z:\.\-)]+) - - \[(.*?)\] "(.*?)" (\d+) ((\d+)|-) ("-")?(.*?) "(.*?)"'
-	print i
-	for line in log_file.readlines():
-		try:
-			search = re.match(regex, line).groups()
-			dateutil.parser.parse(search[1].replace(':', ' ', 1))
-		except :
-			print line 
+class LogAnalysis:
+	def __init__(self):
+		self.client = MongoClient()
+		self.db = self.client.project_test
+	def log_insert(self, collection_name, date):
+		collection = self.db[collection_name]
+		collection.insert(date)
+	def read_apache_logs(self):
+		fileds = ['clien_ip','date','request','status','request_size','browser_string']
+		for i in range(1,5):
+			log_file = open("access_log_"+str(i),"r")
+			regex = '([(\da-zA-Z:\.\-)]+) - - \[(.*?)\] "(.*?)" (\d+) ((\d+)|-) ("-")?(.*?) "(.*?)"'
+			print i
+			for line in log_file.readlines()[:20]:
+				try:
+					search = re.match(regex, line).groups()
+					date = dateutil.parser.parse(search[1].replace(':', ' ', 1))
+					date = (datetime(date.year,date.month,date.day,date.hour,date.minute,date.second),)
+					path = (search[2].split()[1],)
+					log_data = dict(zip(fileds,search[:1]+date+path+search[3:5]+search[8:]))
+					self.log_insert('access_log',log_data)
+				except :
+					print line
+la = LogAnalysis()
+la.read_apache_logs()
