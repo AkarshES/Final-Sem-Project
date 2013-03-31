5508289a4fb63b87a2f208f72da1e83aabe70c53
Akarsh E S <akarsh.es@gmail.com>
2013-03-31 11:29:46 +0530

    First working pandas analysis, median

--- parse_apache.py
+++ parse_apache.py
@@ -4,6 +4,7 @@ import os
 import dateutil.parser
 from datetime import datetime
 from pymongo import MongoClient
+from pandas import DataFrame
 
 class LogAnalysis:
 	def __init__(self):
@@ -12,21 +13,42 @@ class LogAnalysis:
 	def log_insert(self, collection_name, date):
 		collection = self.db[collection_name]
 		collection.insert(date)
-	def read_apache_logs(self):
-		fileds = ['clien_ip','date','request','status','request_size','browser_string']
-		for i in range(1,5):
-			log_file = open("access_log_"+str(i),"r")
-			regex = '([(\da-zA-Z:\.\-)]+) - - \[(.*?)\] "(.*?)" (\d+) ((\d+)|-) ("-")?(.*?) "(.*?)"'
-			print i
-			for line in log_file.readlines()[:20]:
-				try:
-					search = re.match(regex, line).groups()
-					date = dateutil.parser.parse(search[1].replace(':', ' ', 1))
-					date = (datetime(date.year,date.month,date.day,date.hour,date.minute,date.second),)
-					path = (search[2].split()[1],)
-					log_data = dict(zip(fileds,search[:1]+date+path+search[3:5]+search[8:]))
-					self.log_insert('access_log',log_data)
-				except :
-					print line
-la = LogAnalysis()
-la.read_apache_logs()
+	
+	def insert_apache_logs(self, file_name):
+		fields = ['client_ip','date','request','status','request_size','browser_string']
+		apache_log_regex = '([(\da-zA-Z:\.\-)]+) - - \[(.*?)\] "(.*?)" (\d+) ((\d+)|-) ("-")?(.*?) "(.*?)"'
+		log_file = open(file_name,"r")
+		for line in log_file.readlines()[:20]:
+			try:
+				search = re.match(apache_log_regex, line).groups()
+				date = dateutil.parser.parse(search[1].replace(':', ' ', 1))
+				date = (datetime(date.year,date.month,date.day,date.hour,date.minute,date.second),)
+				path = (search[2].split()[1],)
+				log_data = dict(zip(fields,search[:1]+date+path+search[3:5]+search[8:]))
+				self.log_insert('access_log',log_data)
+			except :
+				print line
+	
+	def load_apache_logs_into_DataFrame(self, collection_name):
+		fields = ['client_ip','date','request','status','request_size','browser_string']
+		collection = self.db[collection_name]
+		log_data = collection.find()
+		return DataFrame(list(log_data),columns = fields)
+	
+		
+	def mean(self, collection_name, mean_of, groupby = None):
+		log_data = self.load_apache_logs_into_DataFrame(collection_name)
+		computed_mean = None
+		try:
+			if(groupby):
+				computed_mean = log_data[mean_of].groupby(log_data[groupby]).median()
+			else:
+				computed_mean = log_data[mean_of].median()
+		except KeyError:
+			print 'Check if the field ' + mean_of + ' exists.'
+		return computed_mean
+
+if __name__ == '__main__':
+	la = LogAnalysis()
+	print la.mean('access_log','request_size')
+	print la.mean('access_log','request_size','status')
