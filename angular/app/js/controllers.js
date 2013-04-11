7c9ff7f59afd8ca350a7ff4bd80e374553e47dcc
ajy <ajygvnkr@gmail.com>
2013-04-11 13:16:05 +0530

    moved sample data to new location

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -2,14 +2,14 @@
 
 /* Controllers */
 function MainCtrl($scope, $http){
-    $http.get('LogData/sample-user-object.json').success(function(response_json, status){
+    $http.get('../sample_data/sample-user-object.json').success(function(response_json, status){
         $scope.User = response_json;
     });
 }
 
 function LogViewerCtrl($scope, $http, $routeParams){
     $scope.table_name = $routeParams.table_name;
-    $http.get('LogData/sample.json').success( function(response_json, status){
+    $http.get('../sample_data/sample-data.json').success( function(response_json, status){
         $scope.LogData = response_json.data;
     });
 }
