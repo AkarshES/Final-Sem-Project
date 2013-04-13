b15aa61d6c13b0289dcde602e1f677b1b04880bb
Akarsh E S <akarsh.es@gmail.com>
2013-04-13 12:33:33 +0530

    Add get_log_date().

--- log_analyzer.py
+++ log_analyzer.py
@@ -53,6 +53,19 @@ class LogAnalyzer:
         log_data = collection.find().limit(50)
         return DataFrame(list(log_data),columns = fields)
 
+    def get_log_data(self, collection_name):
+        fields = ['client_ip','date','request','status','request_size','browser_string']
+        if collection_name not in self.db.collection_names():
+            return False
+        collection = self.db[collection_name]
+        log_data = collection.find().limit(50)
+        log_list = []
+        for log in log_data:
+            log_list.append(log)
+            log['date'] = log['date'].strftime("%s")
+            log.pop('_id')
+        return json.dumps({"data" : log_list})
+
     def median(self, df, mean_of, group_by = None):
         computed_mean = None
         try:
@@ -69,8 +82,10 @@ class LogAnalyzer:
 
     def count(self, df, field):
         return df[field].count()
+    
     def group_by(self, df, field):
         return df.groupby(df[field])
+    
     def count_hits(self, collection_name):
         df = self.load_apache_logs_into_DataFrame(collection_name)
         if df is False:
@@ -80,7 +95,7 @@ class LogAnalyzer:
         counts_list = []
         for row in count_data.index:
             counts_list.append({"date" : datetime(row[0][0],row[0][1],row[0][2]).strftime("%s"), "status" : row[1], "count" : str(count_data[row])})
-        return json.dumps(counts_list)
+        return json.dumps({"data" : counts_list})
         # json_data = pandasjson.to_json(self.count(df, 'request_size'))
         # json_load = json.loads(json_data)
         # new_json_dict = []
@@ -93,4 +108,5 @@ if __name__ == '__main__':
     # lp = LogParser()
     # lp.load_apache_log_file_into_DB('access_log_3','access_log_3')
     la = LogAnalyzer()
-    print la.count_hits('access_log_1')
+    print la.get_log_data('access_log_1')
+    # print la.count_hits('access_log_1')
